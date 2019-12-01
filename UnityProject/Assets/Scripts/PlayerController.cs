e8691ff6e0b94ae407cce68e187bef850960e029
KID <50666207+KID421@users.noreply.github.com>
2019-12-01 11:50:53 +0800

    取得動畫資訊並利用名稱判定是否關閉第三人稱控制器

--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,23 @@
 ﻿using UnityEngine;
+using Invector.CharacterController;
 
 public class PlayerController : MonoBehaviour
 {
-    private Animator ani, aniRoot;  // 模型動畫控制器，工具動畫控制器
+    private Animator ani, aniRoot;          // 模型動畫控制器，工具動畫控制器
+    private vThirdPersonController tpc;     // 第三人稱控制器
 
     private void Start()
     {
         ani = GetComponent<Animator>();
-        aniRoot = transform.root.GetComponent<Animator>();  // 根物件 transform.root
+        aniRoot = transform.root.GetComponent<Animator>();          // 根物件 transform.root
+        tpc = transform.root.GetComponent<vThirdPersonController>();
     }
 
     private void Update()
     {
         Move();
         Jump();
+        Attack();
     }
 
     private void Move()
@@ -21,8 +25,8 @@ public class PlayerController : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");     // 左右、AD
         float v = Input.GetAxisRaw("Vertical");       // 上下、WS
 
-        print("左右：" + h);
-        print("上下：" + v);
+        //print("左右：" + h);
+        //print("上下：" + v);
 
         ani.SetBool("走路開關", h != 0 || v != 0);
         ani.SetBool("跑步開關", Input.GetKey(KeyCode.LeftShift));
@@ -35,6 +39,19 @@ public class PlayerController : MonoBehaviour
 
     private void Attack()
     {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ani.SetTrigger("攻擊觸發");
+        }
 
+        // 取得動畫狀態資訊.動畫名稱是否為 ""
+        if (ani.GetCurrentAnimatorStateInfo(0).IsName("攻擊"))
+        {
+            tpc.enabled = false;
+        }
+        else
+        {
+            tpc.enabled = true;
+        }
     }
 }
