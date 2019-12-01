7343efcababfeffff68fb354014e02f45e37944c
KID <50666207+KID421@users.noreply.github.com>
2019-12-01 15:47:29 +0800

    使用判斷式 switch 搭配列舉 enum 判斷 NPC 狀態

--- UnityProject/Assets/Scripts/NPC.cs
+++ UnityProject/Assets/Scripts/NPC.cs
@@ -11,8 +11,16 @@ public class NPC : MonoBehaviour
     public string dialogStart = "陌生人你好，請問可以幫我找到十個零件嗎？";
     public string dialogNotComplete = "請問你還沒找到十個零件嗎？";
     public string dialogComplete = "陌生人，謝謝你幫我找到十個零件。";
-    [Header("對話速度"), Range(0.1f, 5.5f)]
+    [Header("對話速度"), Range(0.001f, 5.5f)]
     public float dialogSpeed = 0.5f;
+
+    // 定義列舉
+    public enum npcState
+    {
+        start, notComplete, complete
+    }
+    // 實例化列舉
+    public npcState _npcState;
 
     /// <summary>
     /// 對話開始
@@ -20,23 +28,53 @@ public class NPC : MonoBehaviour
     private void DialogStart()
     {
         dialog.SetActive(true);
-        StartCoroutine(ShowDialog());
+        StopAllCoroutines();                        // 停止所有協同程序
+
+        switch (_npcState)
+        {
+            case npcState.start:
+                StartCoroutine(ShowDialog(dialogStart));
+                break;
+            case npcState.notComplete:
+                StartCoroutine(ShowDialog(dialogNotComplete));
+                break;
+            case npcState.complete:
+                StartCoroutine(ShowDialog(dialogComplete));
+                break;
+        }
+
+        /**
+        int lv = 1;
+        // 判斷式 switch
+        switch (lv)
+        {
+            case 1:
+                print("1 等");
+                break;
+            case 10:
+                print("10 等");
+                break;
+            default:
+                print("未定義");
+                break;
+        }
+        */
     }
 
     /// <summary>
     /// 顯示對話內容：打字效果
     /// </summary>
     /// <returns></returns>
-    private IEnumerator ShowDialog()
+    private IEnumerator ShowDialog(string dialog)
     {
-        textDialog.text = dialogStart[0].ToString();
-        yield return new WaitForSeconds(dialogSpeed);
-        textDialog.text += dialogStart[1].ToString();
-        yield return new WaitForSeconds(dialogSpeed);
-        textDialog.text += dialogStart[2].ToString();
-        yield return new WaitForSeconds(dialogSpeed);
-        textDialog.text += dialogStart[3].ToString();
-        yield return new WaitForSeconds(dialogSpeed);
+        textDialog.text = "";
+
+        // for (初始值；條件(布林值)；迭代器
+        for (int i = 0; i < dialog.Length; i++)
+        {
+            textDialog.text += dialog[i].ToString();
+            yield return new WaitForSeconds(dialogSpeed);
+        }
     }
 
     /// <summary>
