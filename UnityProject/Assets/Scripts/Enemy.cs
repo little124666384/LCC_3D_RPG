a1ffe9834d0df56edca9bf76c215838303eb0196
KID <50666207+KID421@users.noreply.github.com>
2019-12-08 15:21:41 +0800

    新增攻擊範圍欄位與繪製攻擊範圍射線

--- UnityProject/Assets/Scripts/Enemy.cs
+++ UnityProject/Assets/Scripts/Enemy.cs
@@ -3,6 +3,7 @@ using UnityEngine.AI;   // 引用 人工智慧 API
 
 public class Enemy : MonoBehaviour
 {
+    #region 欄位
     [Header("基本欄位")]
     public float atk = 20;
     public float hp = 250;
@@ -12,14 +13,18 @@ public class Enemy : MonoBehaviour
     public float distanceAttack = 5f;
     [Range(0f, 100f), Tooltip("追蹤距離")]
     public float distanceTrack = 25f;
-    [Range(2.5f, 5f)]
+    [Range(2.5f, 5f), Tooltip("每次攻擊冷卻時間")]
     public float cd = 3.5f;
+    [Range(2.5f, 5f), Tooltip("攻擊範圍")]
+    public float rangeAttack = 3f;
 
     private float timer;
     private Transform target;   // 目標物件
     private Animator ani;       // 動畫元件
     private NavMeshAgent agent; // 導覽代理器元件
+    #endregion
 
+    #region 事件
     private void Start()
     {
         ani = GetComponent<Animator>();
@@ -40,8 +45,16 @@ public class Enemy : MonoBehaviour
 
         Gizmos.color = new Color(0, 1, 0, 0.3f);
         Gizmos.DrawSphere(transform.position, distanceTrack);
-    }
 
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position + Vector3.up, transform.forward * rangeAttack);  // forward : 前方 Z、right : 右方 X、up : 上方 Y
+    }
+    #endregion
+
+    #region 方法
+    /// <summary>
+    /// 追蹤：用距離判斷玩家位置並切換狀態 - 攻擊、追蹤與等待
+    /// </summary>
     private void Track()
     {
         float dis = Vector3.Distance(target.position, transform.position);  // 距離 = 三維向量.距離(A 點，B 點)
@@ -61,11 +74,17 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 等待狀態：等待動畫與停止代理器
+    /// </summary>
     private void Idle()
     {
         ani.SetBool("走路開關", !(agent.isStopped = true));                 // 是否停止 = 否 - 動畫狀態 = 顛倒(是否停止)
     }
 
+    /// <summary>
+    /// 攻擊狀態：啟動計時器、判斷時間並播放攻擊動畫
+    /// </summary>
     private void Attack()
     {
         if (timer >= cd)                                                    // 如果 計時器 >= 冷卻時間
@@ -90,4 +109,5 @@ public class Enemy : MonoBehaviour
     {
 
     }
+    #endregion
 }
